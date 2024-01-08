f2d5c0865e65b7528316d903aee5960d9b5afa80
sergio.noriega <crgiosk.23@gmail.com>
2024-01-07 20:37:55 -0500

    Config ui

--- app/src/main/java/com/example/realtimebasico/MainActivity.kt
+++ app/src/main/java/com/example/realtimebasico/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.realtimebasico
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.realtimebasico.data.FirebaseInstance
+import com.example.realtimebasico.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var firebaseInstance: FirebaseInstance
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        firebaseInstance = FirebaseInstance(this)
+
+        setupUI()
+    }
+
+    private fun setupUI() {
+        binding.btnUpdate.setOnClickListener {
+            firebaseInstance.writeOnFirebase()
+        }
     }
 }
